3da7524289966c1a144d8522e6f5990da815541d
Luis Acuna <mendez97.luca@gmail.com>
2023-11-17 23:19:30 -0800

    done

--- client/src/State.tsx
+++ client/src/State.tsx
@@ -6,6 +6,10 @@ import { PianoInstrument } from './instruments/Piano';
 import { WaveformVisualizer } from './visualizers/Waveform';
 import { HarpInstrument } from './instruments/Harp';
 import { FluteInstrument } from './instruments/Flute';
+import { DrumInstrument } from './instruments/drum';
+
+
+import { EllipticalVisualizer } from './visualizers/drums'
 
 /** ------------------------------------------------------------------------ **
  * The entire application state is stored in AppState.
@@ -16,13 +20,13 @@ export type AppState = Map<string, any>; // similar to { [id: string]: any }
  * Start with the default piano instrument.
  * Add your instruments to this list.
  */
-const instruments = List([PianoInstrument, HarpInstrument, FluteInstrument]); // similar to Instrument[]
+const instruments = List([PianoInstrument, HarpInstrument, FluteInstrument, DrumInstrument]); // similar to Instrument[]
 
 /**
  * Start with the default waveform visualizer.
  * Add your visualizers to this list.
  */
-const visualizers = List([WaveformVisualizer]); // similar to Visualizer[]
+const visualizers = List([WaveformVisualizer, EllipticalVisualizer]); // similar to Visualizer[]
 
 /**
  * The default application state contains a list of instruments and a list of visualizers.
