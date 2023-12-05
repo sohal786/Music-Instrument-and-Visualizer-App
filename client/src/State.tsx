32dc580fe40e793d60cbb742364e00ae1453ba61
jooho2055 <jooho2055@gmail.com>
2023-12-05 00:24:48 -0800

    update

--- client/src/State.tsx
+++ client/src/State.tsx
@@ -4,17 +4,14 @@ import { List, Map } from 'immutable';
 // project dependencies
 import { PianoInstrument } from './instruments/Piano';
 import { WaveformVisualizer } from './visualizers/Waveform';
-
-import { HarpInstrument } from './instruments/Harp';
+import { TriangleVisualizer } from './visualizers/jooho2055';
+import { HarpInstrument } from './instruments/jooho2055';
 import { FluteInstrument } from './instruments/Flute';
 import { DrumInstrument } from './instruments/drum';
 
 import { GuitarInstrument } from './instruments/Guitar';
 
-
-
-
-import { EllipticalVisualizer } from './visualizers/drums'
+import { EllipticalVisualizer } from './visualizers/drums';
 import { multipleVisualizer } from './visualizers/itsAhmato';
 
 /** ------------------------------------------------------------------------ **
@@ -26,14 +23,25 @@ export type AppState = Map<string, any>; // similar to { [id: string]: any }
  * Start with the default piano instrument.
  * Add your instruments to this list.
  */
-const instruments = List([PianoInstrument, HarpInstrument, FluteInstrument, DrumInstrument, GuitarInstrument]); // similar to Instrument[]
-      // similar to Instrument[]
+const instruments = List([
+	PianoInstrument,
+	HarpInstrument,
+	FluteInstrument,
+	DrumInstrument,
+	GuitarInstrument,
+]); // similar to Instrument[]
+// similar to Instrument[]
 
 /**
  * Start with the default waveform visualizer.
  * Add your visualizers to this list.
  */
-const visualizers = List([WaveformVisualizer, EllipticalVisualizer, multipleVisualizer]); // similar to Visualizer[]
+const visualizers = List([
+	WaveformVisualizer,
+	EllipticalVisualizer,
+	multipleVisualizer,
+	TriangleVisualizer,
+]); // similar to Visualizer[]
 
 /**
  * The default application state contains a list of instruments and a list of visualizers.
